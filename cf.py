f233bd7ef6821721dc4972fbd2ff122e7f4e4d32
ajal88 <amirabbasjl@yahoo.com>
2017-11-05 20:34:15 +0330

    user topic matrix

--- cf.py
+++ cf.py
@@ -1,4 +1,5 @@
 import lda
+import numpy
 
 
 def user_news_creator():
@@ -6,25 +7,28 @@ def user_news_creator():
     user_news_file = file.read().split('\n')
     news = int(user_news_file[0])
 
-    user_news = dict()
+    user_news = list()
+    user_id = dict()
     for i in range(1, len(user_news_file)):
         data = user_news_file[i].split(' ')
-        user_news[data[0]] = data[1:news]
+        user_id[data[0]] = i
+        user_news.append(data[1:news])
 
-    return user_news
+    return user_news, user_id
 
 
 # read from written file
 def news_topic_creator():
     file = open('news_topic_matrix.txt', 'r')
     news_topic_file = file.read().split('\n')
-    news_topic = dict()
+    news_topic = list()
     for i in range(len(news_topic_file)):
         num = news_topic_file[i].split(' ')
 
         if len(num) != 0:
-            news_topic[i] = num
-        # list is string not float convert if needed
+            news_topic.append(num)
+
+    # list is string not float convert if needed
 
     return news_topic
 
@@ -34,17 +38,34 @@ def add_new_doc_to_news_topic(doc_name):
     file = open('news_topic_matrix.txt', 'a+')
     news_topic = lda.news_topic_creator('news2')
     log = ''
-    for item in news_topic:
-        log += str(item) + ' '
+    for item_x in news_topic:
+        log += str(item_x) + ' '
     file.write(log)
     file.close()
 
 
+'''
+def user_topic_reader():
+    file = open('user_topic_matrix.txt', 'r')
+    user_topic_file = file.read().split('\n')
+    news = int(user_topic_file[0])
+    # TODO map id to cookie
+    user_news = dict()
+    for i in range(1, len(user_news_file)):
+        data = user_news_file[i].split(' ')
+        user_news[data[0]] = data[1:news]
+
+    return user_news
+'''
+
 if __name__ == '__main__':
     # TODO make file
-    # user_news_matrix = user_news_creator()
+    user_news_matrix, user_id = user_news_creator()
+    user_news_matrix = numpy.array(user_news_matrix)
     news_topic_matrix = news_topic_creator()
-    for item in news_topic_matrix:
-        print(item, ":", news_topic_matrix[item])
+    news_topic_matrix = numpy.array(news_topic_matrix)
 
-    print("hi")
+    user_topic_matrix = numpy.dot(user_news_matrix, news_topic_matrix.T)
+
+    for item in user_news_matrix:
+        print(item, ":", user_news_matrix[item])
---
+++ fileMaker.py
@@ -0,0 +1,23 @@
+import random
+
+
+def user_news_maker(user, news):
+    file = open('user_news_matrix.txt', 'a+', encoding='utf-8')
+    log = ''
+    log += str(news) + '\n'
+    for i in range(0, user):
+        news_rank = [0] * news
+        count = random.randint(0, news)
+        for j in range(0, count):
+            news_index = random.randint(0, news - 1)
+            news_rank[news_index] = round(random.uniform(0, 100), 2)
+
+        log += str(i) + ' '
+        for k in news_rank:
+            log += str(k) + ' '
+        log += '\n'
+    file.write(log)
+    file.close()
+
+if __name__ == "__main__":
+    user_news_maker(5, 5)
