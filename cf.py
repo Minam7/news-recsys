a99a2a1d14498dec493d930a19422fb822f23ebf
ajal88 <amirabbasjl@yahoo.com>
2017-11-06 18:49:31 +0330

    add recommendation method

--- cf.py
+++ cf.py
@@ -23,8 +23,12 @@ def user_news_creator():
 
 
 # read from written file
-def user_topic_creator():
-    file = open('files/user_topic_matrix.txt', 'r')
+def user_topic_creator(norm):
+    if norm:
+        file = open('files/user_topic_matrix_normal.txt', 'r')
+    else:
+        file = open('files/user_topic_matrix.txt', 'r')
+
     user_topic_file = file.read().split('\n')
     user_topic = list()
     for i in range(len(user_topic_file)):
@@ -50,6 +54,17 @@ def news_topic_creator():
             news_topic.append(num)
 
     return news_topic
+
+
+def user_news_topic_creator():
+    user_topic = user_topic_creator(False)
+    user_count = len(user_topic)
+    news_topic = news_topic_creator()
+    news_count = len(news_topic)
+    un_topic = list(user_topic)
+    un_topic.extend(news_topic)
+
+    return un_topic, news_count, user_count
 
 
 def add_new_doc_to_news_topic(doc_name):
@@ -99,7 +114,7 @@ def user_topic_matrix(user_news_matris):
 
     nR = numpy.dot(nP, nQ.T)
 
-    file = open('files/user_topic_matrix.txt', 'w')
+    file = open('files/user_topic_matrix_normal.txt', 'w')
     log = ''
     for j in range(len(nR)):
         item = nR[j]
@@ -112,35 +127,100 @@ def user_topic_matrix(user_news_matris):
     file.write(log)
     file.close()
 
+    file = open('files/user_topic_matrix.txt', 'w')
+    log = ''
+    for j in range(len(user_topic_matris)):
+        item = user_topic_matris[j]
+        for i in range(len(item)):
+            log += str(item[i])
+            if i < len(item) - 1:
+                log += ' '
+        if j < len(user_topic_matris) - 1:
+            log += '\n'
+    file.write(log)
+    file.close()
+
     return nR, user_topic_matris
 
 
 def user_recommendation(user_cookie):
     user_news_matrix, user_id = user_news_creator()
     user = user_id[user_cookie]
+    print("For user with user id : {} and user cookie : {}".format(user, user_cookie))
     nR, utm = user_topic_matrix(user_news_matrix)
     # nR is a mf matrix and utm is raw matrix
 
     # you can use utm or nR!
-    utm = sparse.csr_matrix(utm)
+    # utm = sparse.csr_matrix(utm)
 
-    similarities = cosine_similarity(utm)
-    print('pairwise dense output:\n {}\n'.format(similarities))
+    unt, n_count, u_count = user_news_topic_creator()
 
-    similarities_sparse = cosine_similarity(utm, dense_output=False)
-    print('pairwise sparse output:\n {}\n'.format(similarities_sparse))
-    pass
+    unt = sparse.csr_matrix(unt)
+    similarities_sparse = cosine_similarity(unt, dense_output=False)
+
+    simi_list = list()
+    for i in range(0, n_count + u_count):
+        if i == user:
+            simi_list.append(0)
+        else:
+            ss = similarities_sparse[user, i]
+            simi_list.append(ss)
+
+    u_f = False
+    n_f = False
+    while not u_f or not n_f:
+        rec = max(simi_list)
+        indx = simi_list.index(rec)
+
+        if indx >= u_count and not n_f:
+            print("Recommended news : ", str(indx - u_count))
+            n_f = True
+            simi_list[indx] = -1
+        if indx < u_count and not u_f:
+            print("Recommended user : ", str(indx))
+            u_f = True
+            simi_list[indx] = -1
+        else:
+            simi_list[indx] = -1
+
+
+def news_recommendation(news_id):
+    print("For news with news id : {}".format(news_id))
+    unt, n_count, u_count = user_news_topic_creator()
+    print(n_count, u_count)
+    unt = sparse.csr_matrix(unt)
+    similarities_sparse = cosine_similarity(unt, dense_output=False)
+    print(similarities_sparse)
+    simi_list = list()
+    for i in range(n_count + u_count):
+        if i == news_id + u_count:
+            simi_list.append(0)
+        else:
+            ss = similarities_sparse[news_id + u_count, i]
+            simi_list.append(ss)
+
+    print(simi_list)
+
+    u_f = False
+    n_f = False
+    while not u_f or not n_f:
+        rec = max(simi_list)
+        indx = simi_list.index(rec)
+
+        if indx < u_count and not u_f:
+            print("Recommended user : ", str(indx))
+            u_f = True
+            simi_list[indx] = -1
+
+        if indx >= u_count and not n_f:
+            print("Recommended news : ", str(indx - u_count))
+            n_f = True
+            simi_list[indx] = -1
+
+        else:
+            simi_list[indx] = -1
 
 
 if __name__ == '__main__':
-    user_news_matrix, user_id = user_news_creator()
-    nR, utm = user_topic_matrix(user_news_matrix)
-    # nR is a mf matrix and utm is raw matrix
-
-    # you can use utm or nR!
-    utm = sparse.csr_matrix(utm)
-
-    similarities = cosine_similarity(utm)
-
-    similarities_sparse = cosine_similarity(utm, dense_output=False)
-    print('pairwise sparse output:\n {}\n'.format(similarities_sparse))
+    user_recommendation('95fba71f-54da-4dae-9125-44e4390b5480')
+    news_recommendation(0)
